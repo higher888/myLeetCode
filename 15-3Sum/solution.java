ddaac234e9239eae9770169aeaaad0389a6e5629
higher888 <hai_jiang123@outlook.com>
2016-06-30 18:12:43 -0400

    [15. 3Sum][Compile Error]committed by LeetCode Extension

---
+++ 15-3Sum/solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public List<List<Integer>> threeSum(int[] numbers) {
+        Arrays.sort(numbers);
+        List<List<Integer>> resultArList = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numbers.length - 2; i++){
+            int j = i + 1;
+            int k = numbers.length - 1;
+            while (j < k){
+                int sum = numbers[i] + numbers[j] + numbers[k];
+                if (sum == 0){
+                    /* because everytime if I clear this small arraylist, the corresponding arrylist in the large one is also cleared*/
+                    /* If i need many arraylist, I need to "new" many times*/
+                    /* whether this would be problem if the name of new ArrayList is the same ---- no problem */
+                    List<Integer> numArList = new ArrayList<Integer>();
+                    numArList.add(numbers[i]);
+                    numArList.add(numbers[j]);
+                    numArList.add(numbers[k]);
+                    if (!resultArList.contains(numArList)){
+                        resultArList.add(numArList);
+                    }
+                    k--;
+                }
+                else if (sum < 0){
+                    j++;
+                }
+                else {
+                    k--;
+                }
+            }
+        }
+        return resultArList;
+}
